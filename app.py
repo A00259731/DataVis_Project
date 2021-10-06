11f0391d448101fffa33a4820ebcd48bd70c642a
Madkyen <kristingreene69@gmail.com>
2021-10-06 10:46:04 +0100

    V.0.1 Added Image for final part of project.
    Added Part 1 Textblob and Part 2 Vader links.
    
    Added Part 1 tweet functionality like results.html

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 
 import tweepy as tp
+import textblob as tb
+import vader as vr;
 
 from twitter_auth import *
 
@@ -19,12 +21,44 @@ def homepage():
 def aboutpage():
     return render_template('about.html')
 
+@app.route('/part1', methods=['POST'])
+def part1():
+    query = 'trump'
+    tweets = tp.Cursor(api.search, q=query).items(10)
+
+    #positive = 0;
+    #negative = 0;
+    #netural = 0;
+    #polarity = 0;
+
+    #for tweet in tweets:
+    #    analysis = tb(tweet.text)
+    #    polarity += analysis.sentiment.polarity
+
+    #    if(analysis.sentiment.polarity == 0):
+    #        netural += 1
+    #    elif(analysis.sentiment.polarity < 0):
+    #        negative += 1
+    #    elif(analysis.sentiment.polarity > 0):
+    #        positive += 1
+    #positive = percentage(positive, 100)
+    #negative = percentage(negative, 100)
+    #neutral = percentage(positive, 100)
+
+    #positive = format(positive, '.2f')
+    #neutral = format(neutral, '.2f')
+    #negative = format(negative, '.2f')
+
+    return render_template('part1.html', query=tweets)
+
+    return render_template('404.html')
+
 @app.route('/tweets', methods=['POST'])
 def tweetspage():  # put application's code here
 
     if request.method == 'POST':
-        query = request.form['Destiny 2']
-        tweets = api.search(query)
+        query = request.form['tweet']
+        tweets = tp.Cursor(api.search, q=query).items(10)
 
         return render_template('results.html', query = tweets)
 
@@ -35,3 +69,6 @@ def tweetspage():  # put application's code here
 
 if __name__ == '__main__':
     app.run()
+
+#def percentage(part, whole):
+    #return 100 * float(part)/float(whole)
