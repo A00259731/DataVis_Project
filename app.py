d36b9c3f624a8f447d1a13a8612eb8760e3017af
Madkyen <kristingreene69@gmail.com>
2021-10-20 10:41:39 +0100

    V.0.5.2
    Changed tweet location stuff to part 2 instead of 1.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 
 import tweepy as tp
+import os
 from textblob import TextBlob
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import pandas as pd
@@ -46,14 +47,16 @@ def part1():
         if (analysis.sentiment.polarity == 0):
             neutral += 1
             sentiment = "neu"
+
         elif (analysis.sentiment.polarity < 0.00):
             negative += 1
             sentiment = "neg"
+
         elif (analysis.sentiment.polarity > 0.00):
             positive += 1
             sentiment = "pos"
 
-        tweet_list.append((tweet.text,sentiment))
+        tweet_list.append((tweet.text, sentiment))
 
     df = pd.DataFrame(tweet_list)
 
@@ -64,9 +67,14 @@ def part1():
     neutral = str(percentage(neutral, 10))
     #print(neutral)
 
-    df.to_csv("output.csv", sep=",")
+    if os.path.exists("/part1textblob.csv"):
+        os.remove("part1textblob.csv")
+        df.to_csv("part1textblob.csv", sep=",")
+    else:
+        df.to_csv("part1textblob.csv", sep=",")
 
-    return render_template('part1.html', pos_result = positive, neg_result = negative, neu_result = neutral, query = tweets)
+
+    return render_template('part1.html', pos_result = positive, neg_result = negative, neu_result = neutral, query = tweets, tlist= tweet_list)
 
     return render_template('404.html')
 
@@ -81,6 +89,19 @@ def part2():
     neutral = 0
     polarity = 0
     tweet_list = []
+    neu_list = []
+    pos_list = []
+    neg_list = []
+    neg_loc_ire = 0
+    neg_loc_eng = 0
+    neg_loc_oth = 0
+    neu_loc_ire = 0
+    neu_loc_eng = 0
+    neu_loc_oth = 0
+    pos_loc_ire = 0
+    pos_loc_eng = 0
+    pos_loc_oth = 0
+    sentiment = ""
 
     for tweet in tweets:
         score = SentimentIntensityAnalyzer().polarity_scores(tweet.text)
@@ -88,13 +109,37 @@ def part2():
         neu = score['neu']
         pos = score['pos']
         comp = score['compound']
+        location = str(tweet.user.location)
+
+        if "Ireland" in location:
+            location = "Ireland"
+        elif "England" in location:
+            location = "England"
+        else:
+            location = "Not given"
 
         if neg > pos:
             negative += 1
+            neu_list.append(location)
+            sentiment = "neg"
         elif pos > neg:
             positive += 1
+            sentiment = "pos"
+            neg_list.append(location)
         elif pos == neg:
             neutral += 1
+            sentiment = "neu"
+            pos_list.append(location)
+
+        tweet_list.append((tweet.text,sentiment,location))
+
+    df = pd.DataFrame(tweet_list)
+
+    if os.path.exists("/part2vader.csv"):
+        os.remove("part2vader.csv")
+        df.to_csv("part2vader.csv", sep=",")
+    else:
+        df.to_csv("part2vader.csv", sep=",")
 
     positive = str(percentage(positive, 10))
     #print(positive)
